369efbc0659e2d929bcd5502beb3d63a8f7e7d36
manjeet <manjeetkarnwal1027@gmail.com>
2024-03-29 07:16:25 +0530

    website completed

---
+++ src/useKey.js
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+
+export function useKey(key, action) {
+  useEffect(
+    function () {
+      function callBack(event) {
+        if (event.code.toLowerCase() === key.toLowerCase()) {
+          action();
+        }
+      }
+
+      document.addEventListener("keydown", callBack);
+
+      return function () {
+        document.removeEventListener("keydown", callBack);
+      };
+    },
+    [key, action]
+  );
+}
