0d9c81f951a7c85625515cf3824db55e8c5df3bb
manjeet <manjeetkarnwal1027@gmail.com>
2024-03-30 23:41:58 +0530

    testing git

--- src/useMovies.js
+++ src/useMovies.js
@@ -20,7 +20,7 @@ export function useMovies(query){
 
           const res = await fetch(
             `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
-            // manjeet
+            // Testing git
             { signal: controller.signal }
           );
 
